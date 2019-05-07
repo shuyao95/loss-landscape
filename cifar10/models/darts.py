61aa33e4d80dd1996abbf889752717a372cf8fe6
shuyao95 <shuyao95@gmail.com>
2019-05-07 09:57:13 +0800

    add darts model

--- cifar10/models/darts.py
+++ cifar10/models/darts.py
@@ -237,16 +237,12 @@ class NetworkCIFAR(nn.Module):
 		self.classifier = nn.Linear(C_prev, num_classes)
 
 	def forward(self, input):
-		logits_aux = None
 		s0 = s1 = self.stem(input)
 		for i, cell in enumerate(self.cells):
 			s0, s1 = s1, cell(s0, s1, drop_prob=0)
-			if i == 2 * self._layers // 3:
-				if self._auxiliary and self.training:
-					logits_aux = self.auxiliary_head(s1)
 		out = self.global_pooling(s1)
 		logits = self.classifier(out.view(out.size(0), -1))
-		return logits, logits_aux
+		return logits
 
 Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
 DARTS_V1 = Genotype(
