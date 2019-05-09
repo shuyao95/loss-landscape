057347a5b27acc686a3d8a0bc2baf26500dc595c
shuyao95 <shuyao95@gmail.com>
2019-05-09 18:56:46 +0800

    add more darts related models

--- cifar10/models/darts.py
+++ cifar10/models/darts.py
@@ -245,6 +245,51 @@ class NetworkCIFAR(nn.Module):
 		return logits
 
 Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
+
+NASNet = Genotype(
+	normal=[('sep_conv_5x5', 1), ('sep_conv_3x3', 0), ('sep_conv_5x5', 0), ('sep_conv_3x3', 0),
+	        ('avg_pool_3x3', 1), ('skip_connect', 0), ('avg_pool_3x3', 0), ('avg_pool_3x3', 0),
+	        ('sep_conv_3x3', 1), ('skip_connecåt', 1),
+	        ],
+	normal_concat=[2, 3, 4, 5, 6],
+	reduce=[('sep_conv_5x5', 1), ('sep_conv_7x7', 0), ('max_pool_3x3', 1), ('sep_conv_7x7', 0),
+	        ('avg_pool_3x3', 1), ('sep_conv_5x5', 0), ('skip_connect', 3), ('avg_pool_3x3', 2),
+	        ('sep_conv_3x3', 2), ('max_pool_3x3', 1),
+	        ],
+	reduce_concat=[4, 5, 6],
+)
+
+AmoebaNet = Genotype(
+	normal=[('avg_pool_3x3', 0), ('max_pool_3x3', 1), ('sep_conv_3x3', 0), ('sep_conv_5x5', 2),
+	        ('sep_conv_3x3', 0), ('avg_pool_3x3', 3), ('sep_conv_3x3', 1), ('skip_connect', 1),
+	        ('skip_connect', 0), ('avg_pool_3x3', 1),
+	        ],
+	normal_concat=[4, 5, 6],
+	reduce=[('avg_pool_3x3', 0), ('sep_conv_3x3', 1), ('max_pool_3x3', 0), ('sep_conv_7x7', 2),
+	        ('sep_conv_7x7', 0), ('avg_pool_3x3', 1), ('max_pool_3x3', 0), ('max_pool_3x3', 1),
+	        ('conv_7x1_1x7', 0), ('sep_conv_3x3', 5),
+	        ],
+	reduce_concat=[3, 4, 6]
+)
+
+SNAS_MILD = Genotype(
+	normal=[('sep_conv_3x3', 0), ('sep_conv_3x3', 1), ('skip_connect', 0), ('dil_conv_3x3', 1),
+	        ('skip_connect', 0), ('skip_connect', 1), ('skip_connect', 0), ('sep_conv_3x3', 1)],
+	normal_concat=[2, 3, 4, 5],
+	reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1),
+	        ('max_pool_3x3', 1), ('skip_connect', 2), ('dil_conv_5x5', 2), ('max_pool_3x3', 0)],
+	reduce_concat=[2, 3, 4, 5])
+
+ENAS = Genotype(
+	normal=[('sep_conv_3x3', 1), ('skip_connect', 1), ('sep_conv_5x5', 1), ('skip_connect', 0),
+	        ('avg_pool_3x3', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('avg_pool_3x3', 1),
+	        ('sep_conv_5x5', 1), ('avg_pool_3x3', 0)],
+	normal_concat=[2, 3, 4, 5, 6],
+	reduce=[('sep_conv_5x5', 0), ('avg_pool_3x3', 1), ('sep_conv_3x3', 1), ('avg_pool_3x3', 1),
+	        ('sep_conv_3x3', 1), ('avg_pool_3x3', 1), ('sep_conv_5x5', 4), ('avg_pool_3x3', 1),
+	        ('sep_conv_3x3', 5), ('sep_conv_5x5', 0)],
+	reduce_concat=[2, 3, 6])
+
 DARTS_V1 = Genotype(
 	normal=[('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 0), ('sep_conv_3x3', 1),
 	        ('skip_connect', 0), ('sep_conv_3x3', 1), ('sep_conv_3x3', 0), ('skip_connect', 2)],
@@ -333,6 +378,18 @@ def darts_v2(auxiliary):
 
 def darts_v1(auxiliary):
 	return NetworkCIFAR(C=36, num_classes=10, layers=20, auxiliary=auxiliary, genotype=DARTS_V1)
+
+def darts_nasnet(auxiliary):
+	return NetworkCIFAR(C=36, num_classes=10, layers=20, auxiliary=auxiliary, genotype=NASNet)
+
+def darts_amoebanet(auxiliary):
+	return NetworkCIFAR(C=36, num_classes=10, layers=20, auxiliary=auxiliary, genotype=AmoebaNet)
+
+def darts_enas(auxiliary):
+	return NetworkCIFAR(C=36, num_classes=10, layers=20, auxiliary=auxiliary, genotype=ENAS)
+
+def darts_snas(auxiliary):
+	return NetworkCIFAR(C=36, num_classes=10, layers=20, auxiliary=auxiliary, genotype=SNAS_MILD)
 
 def darts_ops1(auxiliary):
 	return NetworkCIFAR(C=36, num_classes=10, layers=20, auxiliary=auxiliary, genotype=DARTS_OPS1)
