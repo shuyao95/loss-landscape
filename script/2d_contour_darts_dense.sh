a13f667566af5f2367d0ee1815c9714eaf27a888
shuyao95 <shuyao95@gmail.com>
2019-05-10 11:22:05 +0800

    add more models

--- script/2d_contour_darts_dense.sh
+++ script/2d_contour_darts_dense.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=$GPU_ID python plot_surface.py --x=-1:1:51 --y=-1:1:51 --mo
 --model_file ../cnn/eval-DARTS_DENSE_NOGROUP-20190506-210531/weights.pt \
 --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter --batch_size 2048
 
-CUDA_VISIBLE_DEVICES=1 python plot_surface.py --x=-1:1:51 --y=-1:1:51 --model dd_nogroup \
+CUDA_VISIBLE_DEVICES=1 python plot_surface.py --x=-1:1:51 --y=-1:1:51 --model darts_nogroup \
 --model_file ../cnn/eval-DARTS_DENSE_NOGROUP-20190506-210531/weights.pt \
 --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter --batch_size 2048
 # darts v2 with cutout
